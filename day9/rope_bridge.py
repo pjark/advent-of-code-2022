e377d2a56f8e47f77d426f55151e09e83cb96394
pjark <jsdp4900@gmail.com>
2022-12-19 12:16:32 -0600

    finished part 1

--- day9/rope_bridge.py
+++ day9/rope_bridge.py
@@ -37,6 +37,39 @@ def print_map(head_loc, tail_loc, input):
     
     return 
 
+def print_map_too(head_loc, knots, input):
+    # calculate a consistent bounds
+    bounds_dict = {
+        'R':0,
+        'L':0,
+        'U':0,
+        'D':0
+    }
+    for direction, steps in input:
+        bounds_dict[direction] += int(steps)
+    
+    bounds_vals = []
+    for key in bounds_dict:
+        bounds_vals.append(bounds_dict[key])
+
+    bounds = max(bounds_vals)
+    bounds = 20
+
+    map = [[] for _ in range(bounds*2)]
+    for x in range(bounds*2):
+        for _ in range(bounds*2):
+            map[x].append(".")
+    
+    map[0+bounds][0+bounds] = 's'
+    for index, knot in enumerate(knots):
+        map[knot[0]+bounds][knot[1]+bounds] = str(index + 1)
+    map[head_loc[0]+bounds][head_loc[1]+bounds] = 'H'
+    
+    for line in map:
+        print(''.join(line))
+    
+    return 
+
 def do_actions(input):
     def move_head(head_loc, move):
         # R = (+1, 0), L = (-1, 0), U = (0, +1), D = (0, -1)
@@ -121,88 +154,7 @@ def do_actions(input):
     return visited
         
 def do_actions_too(input):
-    def move_head(head_loc, move):
-        # R = (+1, 0), L = (-1, 0), U = (0, +1), D = (0, -1)
-        if move == 'R':
-            head_loc[1] += 1
-        elif move == 'L':
-            head_loc[1] -= 1
-        elif move == 'U':
-            head_loc[0] -= 1
-        elif move == 'D':
-            head_loc[0] += 1
-        else:
-            sys.exit("[ERROR: Veronica] Unknown move")
-
-        return head_loc
-    
-    def adjacent(head_loc, tail_loc):
-        # adjacent can be any position touching the head_loc
-        if head_loc[0] == tail_loc[0]:
-            if abs(head_loc[1] - tail_loc[1]) == 1:
-                return True
-        elif head_loc[1] == tail_loc[1]:
-            if abs(head_loc[0] - tail_loc[0]) == 1:
-                return True
-        elif abs(head_loc[0] - tail_loc[0]) + abs(head_loc[1] - tail_loc[1]) == 2:
-            return True
-        else: return False
-
-    def move_tail(head_loc, old_tail_loc):
-        if head_loc == old_tail_loc:
-            return old_tail_loc
-
-        new_tail_loc = -1
-        if head_loc[0] == old_tail_loc[0] or head_loc[1] == old_tail_loc[1]:
-            possible_new_locs = [
-                [old_tail_loc[0]+1, old_tail_loc[1]],
-                [old_tail_loc[0]-1, old_tail_loc[1]],
-                [old_tail_loc[0], old_tail_loc[1]+1],
-                [old_tail_loc[0], old_tail_loc[1]-1]
-            ]
-            for possible_new_loc in possible_new_locs:
-                if adjacent(head_loc, possible_new_loc):
-                    new_tail_loc = possible_new_loc
-        elif math.dist(head_loc, old_tail_loc) > 2.0:
-            possible_new_locs = [
-                [old_tail_loc[0]-1, old_tail_loc[1]-1],
-                [old_tail_loc[0]-1, old_tail_loc[1]+1],
-                [old_tail_loc[0]+1, old_tail_loc[1]-1],
-                [old_tail_loc[0]+1, old_tail_loc[1]+1]
-            ]
-            for possible_new_loc in possible_new_locs:
-                if adjacent(head_loc, possible_new_loc):
-                    new_tail_loc = possible_new_loc
-
-        # sanity checking
-        if new_tail_loc == -1:
-            print("\n[Error: Martha] Adjacent position somehow not found!")
-            print("-> head @", head_loc, "& tail @", old_tail_loc)
-            print("-> map:"); print_map(head_loc, old_tail_loc)
-            sys.exit("\n")
-        if not adjacent(head_loc, new_tail_loc):
-            print("\n[Error: Steve] Tail was not correctly moved!")
-            print("\thead @", head_loc)
-            print("\ttail moved from", old_tail_loc, "to", new_tail_loc)
-            sys.exit("\n")
-        else:
-            return new_tail_loc
-
-    visited = set(); visited.add(tuple([0,0]))
-    head_loc = [0,0]
-    knots = [[0,0] for _ in range(9)]
-    for line in input:
-        direction, steps = line
-        movement_queue = [direction for _ in range(int(steps))]
-        for move in movement_queue:
-            head_loc = move_head(head_loc, move)
-            leader = head_loc
-            for index, follower in enumerate(knots):
-                if not adjacent(leader, follower):
-                    knots[index] = move_tail(leader, follower)
-            visited.add(tuple(knots[-1]))
-
-    return visited
+    return None
 
 def part2(): # 9 knots (knot #9 = tail)
     input = get_formatted_input(sys.argv[1]); # [print(line) for line in input]
